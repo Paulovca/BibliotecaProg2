f08bb159cee95e0944c991f60d25f7239d1e991f
Paulo Vinícius <Paulo Vincius@192.168.10.106>
2019-04-12 20:29:56 -0300

    Classe teste que faltou, andre tbm.

--- src/teste/classeTeste.java
+++ src/teste/classeTeste.java
@@ -1,11 +1,21 @@
 package teste;
 
+import negocio.Aluno;
+import negocio.Emprestimo;
+import negocio.Funcionario;
+import negocio.Item;
+import negocio.Livro;
+
 public class classeTeste {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		
-		System.out.println("Ola Mundo!!!");
+		Aluno aluno1 = new Aluno("02408324262", "André");
+		Livro livro1 = new Livro("O código davinci", "Paulo Vinícius", 3);
+		Funcionario fun1 = new Funcionario("11111111111", "Diego");
+		Item item1 = new Item(livro1, 1);
+		//deve-se criar um array de itens para passar em empréstimo, porém onde esse array será criado?
+		//deve-se criar uma data de emprestimo e devolucao para passar em empréstimo, porém onde esse objeto data será criada?
+		Emprestimo emp1 = new Emprestimo(aluno1, null, fun1, null, null);
 		
 	}
 	
