8ab059eafc35555faa50997c74839d3d5f36729c
Paulo Vinícius <Paulo Vincius@192.168.10.102>
2019-05-13 00:11:35 -0300

    Controle e repositorio livro feito.

--- src/dados/RepositorioLivrosArray.java
+++ src/dados/RepositorioLivrosArray.java
@@ -1,17 +1,62 @@
 package dados;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import negocio.entidades.Livro;
 
-import negocio.entidades.Aluno;
-import negocio.entidades.Turma;
+public class RepositorioLivrosArray implements RepositorioLivros{
+	
+	private static RepositorioLivrosArray instance;
+	private Livro[] livros;
+	private int indice;
+	
+	private final static int TAMANHO = 200;
+	
+	private RepositorioLivrosArray() {
+		this.livros = new Livro[TAMANHO];
+		this.indice = 0;
+	}
+	
+	public static RepositorioLivrosArray getInstance() {
+		if(instance == null) {
+			instance = new RepositorioLivrosArray();
+		}
+		return instance;
+	}
+	
+	@Override
+	public void cadastrar(Livro livro) {
+		livros[indice] = livro;
+		indice++;
+	}
 
-public class RepositorioLivrosArray implements RepositorioLivros, Serializable {
+	@Override
+	public Livro consultar(int id) {
+		Livro livro = null;
+		for(int i = 0; i< indice ; i++) {
+			if(livros[i].getId() == id) {
+				livro = livros[i];
+			}
+		}
+		return livro;
+	}
+
+	@Override
+	public void remover(int id) {
+		for(int i = 0; i < indice ; i++) {
+			if(livros[i].getId() == id) {
+				livros[i] = livros[indice];
+				livros[indice] = null;
+			}
+		}
+	}
+
+	@Override
+	public void atualizar(Livro livro) {
+		for(int i = 0; i < indice ; i++) {
+			if(livros[i].getId() == livro.getId()) {
+				livros[i] = livro;
+			}
+		}
+	}
 
 	
 }
--- src/negocio/controle/ControleLivros.java
+++ src/negocio/controle/ControleLivros.java
@@ -1,5 +1,48 @@
 package negocio.controle;
 
-public class ControleLivros {
+import dados.RepositorioLivros;
+import dados.RepositorioLivrosArray;
+import negocio.entidades.Livro;
 
+public class ControleLivros {
+		
+	private RepositorioLivros livros = RepositorioLivrosArray.getInstance();
+	
+	public void cadastrar(Livro livro) {
+		if(livro.getTitulo() != null && livro.getAutor() != null) {
+			if(livros.consultar(livro.getId()) == null) {
+				livros.cadastrar(livro);
+			}else {
+				//Exception ja existe
+			}
+		}else {
+			//exception campos nulos
+		}
+	}
+	
+	public void remover(int id) {
+		Livro livro = livros.consultar(id);
+		if(livro == null) {
+			//exception de nao encontrado
+		} else {
+			livros.remover(id);
+		}
+	}
+	
+	public void atualizar(Livro livro) {
+		Livro livroAux = livros.consultar(livro.getId());
+		if(livro == null || livroAux == null) {
+			//exception de não encontrado
+		} else {
+			livros.atualizar(livro);
+		}
+	}
+	
+	public Livro consultar(int id) {
+		Livro livro = livros.consultar(id);
+		if( livro == null) {
+			//exception aluno nao encontrado
+		}
+		return livro;
+	}
 }
