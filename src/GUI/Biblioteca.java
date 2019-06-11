516bfd08c1ddf6341162ca447dd5e9b318482233
andre.porto <andre.porto@LAB-DEINFO-04.deinfo.lab33>
2019-06-11 19:41:46 -0300

    GUI Biblioteca organizada e padrão flag implementado

--- src/GUI/Biblioteca.java
+++ src/GUI/Biblioteca.java
@@ -21,6 +21,7 @@ public class Biblioteca extends JFrame {
 	private JPanel contentPane;
 	private static Biblioteca instance;
 	public static Boolean flag = false;
+
 	/**
 	 * Launch the application.
 	 */
@@ -41,12 +42,12 @@ public class Biblioteca extends JFrame {
 	 * Create the frame.
 	 */
 	public static Biblioteca getInstance() {
-		if(instance == null) {
+		if (instance == null) {
 			instance = new Biblioteca();
 		}
 		return instance;
 	}
-	
+
 	private Biblioteca() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 460, 329);
@@ -54,103 +55,26 @@ public class Biblioteca extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
+		// Aluno----------------------------------------------------------------------------------------------------------------------------
+
 		JLabel lblAluno = new JLabel("Aluno");
 		lblAluno.setFont(new Font("Tahoma", Font.BOLD, 11));
 		lblAluno.setForeground(new Color(255, 255, 255));
 		lblAluno.setBounds(10, 32, 46, 14);
 		contentPane.add(lblAluno);
-		
-		JLabel lblLivro = new JLabel("Livro");
-		lblLivro.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblLivro.setForeground(new Color(255, 255, 255));
-		lblLivro.setBounds(91, 32, 46, 14);
-		contentPane.add(lblLivro);
-		
-		JLabel lblFuncionario = new JLabel("Funcionario");
-		lblFuncionario.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblFuncionario.setForeground(new Color(255, 255, 255));
-		lblFuncionario.setBounds(182, 32, 71, 14);
-		contentPane.add(lblFuncionario);
-		
-		JLabel lblEmprstimo = new JLabel("Empr\u00E9stimo");
-		lblEmprstimo.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblEmprstimo.setForeground(new Color(255, 255, 255));
-		lblEmprstimo.setBounds(273, 32, 71, 14);
-		contentPane.add(lblEmprstimo);
-		
-		JLabel lblDevoluo = new JLabel("Devolu\u00E7\u00E3o");
-		lblDevoluo.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblDevoluo.setForeground(new Color(255, 255, 255));
-		lblDevoluo.setBounds(364, 32, 71, 14);
-		contentPane.add(lblDevoluo);
-		
-		JButton btnVoltar = new JButton("voltar");
-		btnVoltar.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		btnVoltar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				dispose();
-				Login.getInstance().setVisible(true);
-			}
-		});
-		btnVoltar.setBounds(10, 256, 89, 23);
-		contentPane.add(btnVoltar);
-		
+
 		JButton btnCadastrarAluno = new JButton("Cadastrar");
 		btnCadastrarAluno.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		btnCadastrarAluno.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				dispose();
+				setVisible(false);
 				CadastroAluno.getInstance().setVisible(true);
 			}
 		});
 		btnCadastrarAluno.setBounds(0, 57, 80, 23);
 		contentPane.add(btnCadastrarAluno);
-		
-		JButton btnCadastrarLivro = new JButton("Cadastrar");
-		btnCadastrarLivro.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		btnCadastrarLivro.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				dispose();
-				CadastroLivro.getInstance().setVisible(true);
-			}
-		});
-		btnCadastrarLivro.setBounds(91, 57, 80, 23);
-		contentPane.add(btnCadastrarLivro);
-		
-		JButton btnCadastrarFuncionario = new JButton("Cadastrar");
-		btnCadastrarFuncionario.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				dispose();
-				CadastroFuncionario.getInstance().setVisible(true);
-			}
-		});
-		btnCadastrarFuncionario.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		btnCadastrarFuncionario.setBounds(182, 57, 80, 23);
-		contentPane.add(btnCadastrarFuncionario);
-		
-		JButton btnCadastrarEmprestimo = new JButton("Cadastrar");
-		btnCadastrarEmprestimo.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				dispose();
-				CadastroEmprestimo.getInstance().setVisible(true);
-			}
-		});
-		btnCadastrarEmprestimo.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		btnCadastrarEmprestimo.setBounds(273, 57, 80, 23);
-		contentPane.add(btnCadastrarEmprestimo);
-		
-		JButton btnCadastrarDevolucao = new JButton("Cadastrar");
-		btnCadastrarDevolucao.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				dispose();
-				CadastroDevolucao.getInstance().setVisible(true);
-			}
-		});
-		btnCadastrarDevolucao.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		btnCadastrarDevolucao.setBounds(364, 57, 80, 23);
-		contentPane.add(btnCadastrarDevolucao);
-		
+
 		JButton btnAtualizarAluno = new JButton("Atualizar");
 		btnAtualizarAluno.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -161,7 +85,51 @@ public class Biblioteca extends JFrame {
 		btnAtualizarAluno.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		btnAtualizarAluno.setBounds(0, 91, 80, 23);
 		contentPane.add(btnAtualizarAluno);
-		
+
+		JButton btnRemoverAluno = new JButton("Remover");
+		btnRemoverAluno.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				flag = false;
+				ConsultarRemoverAluno.getInstance().setVisible(true);
+
+			}
+		});
+		btnRemoverAluno.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		btnRemoverAluno.setBounds(0, 125, 80, 23);
+		contentPane.add(btnRemoverAluno);
+
+		JButton btnConsultarAluno = new JButton("Consultar");
+		btnConsultarAluno.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				flag = true;
+				ConsultarRemoverAluno.getInstance().setVisible(true);
+			}
+		});
+		btnConsultarAluno.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		btnConsultarAluno.setBounds(0, 159, 80, 23);
+		contentPane.add(btnConsultarAluno);
+
+		// Livro-----------------------------------------------------------------------------------------------------------------------------
+
+		JLabel lblLivro = new JLabel("Livro");
+		lblLivro.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblLivro.setForeground(new Color(255, 255, 255));
+		lblLivro.setBounds(91, 32, 46, 14);
+		contentPane.add(lblLivro);
+
+		JButton btnCadastrarLivro = new JButton("Cadastrar");
+		btnCadastrarLivro.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		btnCadastrarLivro.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				CadastroLivro.getInstance().setVisible(true);
+			}
+		});
+		btnCadastrarLivro.setBounds(91, 57, 80, 23);
+		contentPane.add(btnCadastrarLivro);
+
 		JButton btnAtualizarLivro = new JButton("Atualizar");
 		btnAtualizarLivro.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -172,6 +140,49 @@ public class Biblioteca extends JFrame {
 		btnAtualizarLivro.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		btnAtualizarLivro.setBounds(91, 91, 80, 23);
 		contentPane.add(btnAtualizarLivro);
+
+		JButton btnRemoverLivro = new JButton("Remover");
+		btnRemoverLivro.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				flag = false;
+				ConsultarRemoverLivro.getInstance().setVisible(true);
+			}
+		});
+		btnRemoverLivro.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		btnRemoverLivro.setBounds(91, 125, 80, 23);
+		contentPane.add(btnRemoverLivro);
+
+		JButton btnConsultarLivro = new JButton("Consultar");
+		btnConsultarLivro.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				flag = true;
+				ConsultarRemoverLivro.getInstance().setVisible(true);
+			}
+		});
+		btnConsultarLivro.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		btnConsultarLivro.setBounds(91, 159, 80, 23);
+		contentPane.add(btnConsultarLivro);
+
+		// Funcionario-------------------------------------------------------------------------------------------------------------------------
+
+		JLabel lblFuncionario = new JLabel("Funcionario");
+		lblFuncionario.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblFuncionario.setForeground(new Color(255, 255, 255));
+		lblFuncionario.setBounds(182, 32, 71, 14);
+		contentPane.add(lblFuncionario);
+
+		JButton btnCadastrarFuncionario = new JButton("Cadastrar");
+		btnCadastrarFuncionario.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				CadastroFuncionario.getInstance().setVisible(true);
+			}
+		});
+		btnCadastrarFuncionario.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		btnCadastrarFuncionario.setBounds(182, 57, 80, 23);
+		contentPane.add(btnCadastrarFuncionario);
 		
 		JButton btnAtualizarFuncionario = new JButton("Atualizar");
 		btnAtualizarFuncionario.addActionListener(new ActionListener() {
@@ -184,34 +195,11 @@ public class Biblioteca extends JFrame {
 		btnAtualizarFuncionario.setBounds(182, 91, 80, 23);
 		contentPane.add(btnAtualizarFuncionario);
 		
-		JButton btnRemoverAluno = new JButton("Remover");
-		btnRemoverAluno.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				setVisible(false);
-				flag = false;
-				ConsultarRemoverAluno.getInstance().setVisible(true);
-				
-			}
-		});
-		btnRemoverAluno.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		btnRemoverAluno.setBounds(0, 125, 80, 23);
-		contentPane.add(btnRemoverAluno);
-		
-		JButton btnRemoverLivro = new JButton("Remover");
-		btnRemoverLivro.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				setVisible(false);
-				ConsultarRemoverLivro.getInstance().setVisible(true);
-			}
-		});
-		btnRemoverLivro.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		btnRemoverLivro.setBounds(91, 125, 80, 23);
-		contentPane.add(btnRemoverLivro);
-		
 		JButton btnRemoverFuncionario = new JButton("Remover");
 		btnRemoverFuncionario.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				setVisible(false);
+				flag = false;
 				ConsultarRemoverFuncionario.getInstance().setVisible(true);
 			}
 		});
@@ -219,55 +207,11 @@ public class Biblioteca extends JFrame {
 		btnRemoverFuncionario.setBounds(182, 125, 80, 23);
 		contentPane.add(btnRemoverFuncionario);
 		
-		JButton btnRemoverEmprestimo = new JButton("Remover");
-		btnRemoverEmprestimo.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				setVisible(false);
-				ConsultarRemoverEmprestimo.getInstance().setVisible(true);
-			}
-		});
-		btnRemoverEmprestimo.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		btnRemoverEmprestimo.setBounds(273, 125, 80, 23);
-		contentPane.add(btnRemoverEmprestimo);
-		
-		JButton btnRemoverDevolucao = new JButton("Remover");
-		btnRemoverDevolucao.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				setVisible(false);
-				ConsultarRemoverDevolucao.getInstance().setVisible(true);
-			}
-		});
-		btnRemoverDevolucao.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		btnRemoverDevolucao.setBounds(364, 125, 80, 23);
-		contentPane.add(btnRemoverDevolucao);
-		
-		JButton btnConsultarAluno = new JButton("Consultar");
-		btnConsultarAluno.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				setVisible(false);
-				flag = true;
-				ConsultarRemoverAluno.getInstance().setVisible(true);
-			}
-		});
-		btnConsultarAluno.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		btnConsultarAluno.setBounds(0, 159, 80, 23);
-		contentPane.add(btnConsultarAluno);
-		
-		JButton btnConsultarLivro = new JButton("Consultar");
-		btnConsultarLivro.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				setVisible(false);
-				ConsultarRemoverLivro.getInstance().setVisible(true);
-			}
-		});
-		btnConsultarLivro.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		btnConsultarLivro.setBounds(91, 159, 80, 23);
-		contentPane.add(btnConsultarLivro);
-		
 		JButton btnConsultarFuncionario = new JButton("Consultar");
 		btnConsultarFuncionario.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				setVisible(false);
+				flag = true;
 				ConsultarRemoverFuncionario.getInstance().setVisible(true);
 			}
 		});
@@ -275,10 +219,42 @@ public class Biblioteca extends JFrame {
 		btnConsultarFuncionario.setBounds(182, 159, 80, 23);
 		contentPane.add(btnConsultarFuncionario);
 		
+		// Emprestimo------------------------------------------------------------------------------------------------------------------------
+		
+		JLabel lblEmprstimo = new JLabel("Empr\u00E9stimo");
+		lblEmprstimo.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblEmprstimo.setForeground(new Color(255, 255, 255));
+		lblEmprstimo.setBounds(273, 32, 71, 14);
+		contentPane.add(lblEmprstimo);
+		
+		JButton btnCadastrarEmprestimo = new JButton("Cadastrar");
+		btnCadastrarEmprestimo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				CadastroEmprestimo.getInstance().setVisible(true);
+			}
+		});
+		btnCadastrarEmprestimo.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		btnCadastrarEmprestimo.setBounds(273, 57, 80, 23);
+		contentPane.add(btnCadastrarEmprestimo);
+		
+		JButton btnRemoverEmprestimo = new JButton("Remover");
+		btnRemoverEmprestimo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				flag = false;
+				ConsultarRemoverEmprestimo.getInstance().setVisible(true);
+			}
+		});
+		btnRemoverEmprestimo.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		btnRemoverEmprestimo.setBounds(273, 125, 80, 23);
+		contentPane.add(btnRemoverEmprestimo);
+		
 		JButton btnConsultarEmprestimo = new JButton("Consultar");
 		btnConsultarEmprestimo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				setVisible(false);
+				flag = true;
 				ConsultarRemoverEmprestimo.getInstance().setVisible(true);
 			}
 		});
@@ -286,10 +262,42 @@ public class Biblioteca extends JFrame {
 		btnConsultarEmprestimo.setBounds(273, 159, 80, 23);
 		contentPane.add(btnConsultarEmprestimo);
 		
+		//Devolucao--------------------------------------------------------------------------------------------------------------------
+
+		JLabel lblDevoluo = new JLabel("Devolu\u00E7\u00E3o");
+		lblDevoluo.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblDevoluo.setForeground(new Color(255, 255, 255));
+		lblDevoluo.setBounds(364, 32, 71, 14);
+		contentPane.add(lblDevoluo);
+
+		JButton btnCadastrarDevolucao = new JButton("Cadastrar");
+		btnCadastrarDevolucao.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				CadastroDevolucao.getInstance().setVisible(true);
+			}
+		});
+		btnCadastrarDevolucao.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		btnCadastrarDevolucao.setBounds(364, 57, 80, 23);
+		contentPane.add(btnCadastrarDevolucao);
+
+		JButton btnRemoverDevolucao = new JButton("Remover");
+		btnRemoverDevolucao.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				flag = false;
+				ConsultarRemoverDevolucao.getInstance().setVisible(true);
+			}
+		});
+		btnRemoverDevolucao.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		btnRemoverDevolucao.setBounds(364, 125, 80, 23);
+		contentPane.add(btnRemoverDevolucao);
+
 		JButton btnConsultarDevolucao = new JButton("Consultar");
 		btnConsultarDevolucao.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				setVisible(false);
+				flag = true;
 				ConsultarRemoverDevolucao.getInstance().setVisible(true);
 			}
 		});
@@ -297,6 +305,20 @@ public class Biblioteca extends JFrame {
 		btnConsultarDevolucao.setBounds(364, 159, 80, 23);
 		contentPane.add(btnConsultarDevolucao);
 		
+		//--------------------------------------------------------------------------------------------------------------------------
+		
+		JButton btnVoltar = new JButton("voltar");
+		btnVoltar.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		btnVoltar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
+				instance = null;
+				Login.getInstance().setVisible(true);
+			}
+		});
+		btnVoltar.setBounds(10, 256, 89, 23);
+		contentPane.add(btnVoltar);
+
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Aluno\\Pictures\\aaa.png"));
 		lblNewLabel.setBounds(0, -17, 445, 308);
